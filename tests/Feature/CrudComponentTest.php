9a724211ee0a278d401c9999c9d00013a7c426db
AndreasElia <andreaselia@live.co.uk>
2022-02-22 18:33:51 +0000

    test

---
+++ tests/Feature/CrudComponentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Notano\Cruddy\Tests\Feature;
+
+use Notano\Cruddy\Tests\TestCase;
+
+class CrudComponentTest extends TestCase
+{
+    public function test_before_create()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_on_create()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_before_update()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_on_update()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_on_delete()
+    {
+        $this->assertTrue(true);
+    }
+}
